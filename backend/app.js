c4047135d4ddc868efea9477221770053850aaf8
袁少怡 <shaoyi@shaoyi.local>
2020-03-06 13:55:42 +0800

    上传 浏览下载上传文件

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
 var fs = require('fs');
+var path = require("path");
 var express = require('express');
-var multer = require('multer')
+var multer = require('multer');
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+var { readFile1, readFile2 } = require("./utils/readFile");
+var {postData} = require("./utils/postData");
+
+
 
 var app = express();
-//var upload = multer({ dest: 'upload/' });
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//app.set('view cache', true);
+
 
 
 var storage = multer.diskStorage({
@@ -11,10 +23,17 @@ var storage = multer.diskStorage({
         cb(null, 'upload/')
     },
     filename: function (req, file, cb) {
+        //console.log(req.body.date);
+        date = req.body.date;
         var singfileArray = file.originalname.split('.');
         var fileExtension = singfileArray[singfileArray.length - 1];
-        cb(null, singfileArray[0] + '-' + Date.now() + "." + fileExtension);
-        console.log(file);
+
+
+
+        cb(null, singfileArray[0] + '-' + date + "." + fileExtension);
+
+        //console.log(file);
+
     }
 })
 
@@ -24,14 +43,67 @@ var upload = multer({
 
 // 文件上传
 app.post('/upload', upload.single("logo"), function (req, res, next) {
+    //console.log(req.file.path);
+    //console.log(readFile1(req.file.path))
+    postData(req.file.path) //向后台发送数据
     res.send({
-        "status": "success"
-    })
+        "status": "上传成功"
+    });
+
+});
+// 表单页面
+app.get('/index', function (req, res, next) {
+    var form = fs.readFileSync('./views/form.html', { encoding: 'utf8' });
+    res.send(form);
 });
 
-app.get('/form', function (req, res, next) {
-    var form = fs.readFileSync('./form.html', { encoding: 'utf8' });
-    res.send(form);
+//获取上传文件列表
+app.get('/file', function (req, res, next) {
+
+    //读取upload文件下上传的文件
+    const dirInfo = fs.readdirSync("./upload/").filter((item)=>item.endsWith("xlsx"));
+    //console.log(dirInfo)
+    let pageNum = 5
+    let pageSum = Math.ceil(dirInfo.length / pageNum);
+
+    let pageIndex = Math.floor((req.query.pageIndex || 1));
+    
+    let list = dirInfo.slice((pageIndex - 1) * pageNum, pageIndex * pageNum);
+    var data = {
+        files: list,
+        pageIndex,
+        pageSum,
+    }
+    res.render("fileList.ejs", data);
+});
+
+//下载页面
+app.get('/download', function (req, res) {
+    // var file = './upload/0-2020-03-09.jpg';
+    //console.log(req.query.name)
+
+    let fileName = req.query.name;
+    let filePath = `./upload/${fileName}`;
+
+    console.log(filePath)
+    fs.exists(filePath, function (exists) {
+        if (exists) {
+            console.log("文件存在")
+            res.download(filePath);
+            // res.send({
+            //     "status": "下载成功"
+            // })
+            
+        }
+        if (!exists) {
+            res.send({
+                "status": "文件不存在"
+            })
+            console.log("文件不存在")
+        }
+    })
+
+
 });
 
 app.listen(3000, () => { console.log("Example app listening on port 3000") });
---
+++ backend/utils/postData.js
@@ -0,0 +1,96 @@
+const fetch = require('node-fetch');
+let { readFile1, readFile2 } = require('./readFile');
+var path = require("path");
+var fs = require("fs");
+
+//用readFile2读取的文件
+var file2 = ["广西外资企业和外贸企业复工情况表", "广西建筑业项目和房地产企业复工情况表"];
+
+function post(url, data) {
+    // Default options are marked with *
+
+    return fetch(url, {
+        body: JSON.stringify(data), // must match 'Content-Type' header
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, same-origin, *omit
+        headers: {
+            'user-agent': 'Mozilla/4.0 MDN Example',
+            'content-type': 'application/json'
+        },
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, cors, *same-origin
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'no-referrer', // *client, no-referrer
+    }).then(response => response.json()) // parses response to JSON
+}
+
+
+
+
+function postData(filePath) {
+        
+        //console.log(filePath.split(/[\/|.|-]/))
+        let key = filePath.split(/[\/|.|-]/)[1];
+        console.log(key)
+
+        if (file2.indexOf(key) === -1) {
+            // 上传可以用readFile1读取的文件
+            new Promise(function (resolve) {
+                if (readFile1(filePath)) {
+                    resolve(readFile1(filePath));
+                }
+
+            }).then((res) => {
+                console.log("*******************")
+                console.log(res)
+                data = {
+                    "data": res,
+                    // "key": String(dirs.indexOf(item)),
+                    key,//使用文件名
+                }
+                console.log(encodeURI("http://47.94.216.106/api/test?key=" + key));
+                // //console.log(key)
+                post(encodeURI("http://47.94.216.106/api/test?key=" + key), data).then((res) => {
+                    console.log(res)
+                }).catch((err) => {
+                    console.log(err)
+                })
+            }).catch((err) => {
+                console.log(err)
+            })
+        }
+        else if (file2.indexOf(key) !== -1) {
+            // 上传可以用readFile2读取的文件
+            new Promise(function (resolve) {
+                if (readFile2(filePath)) {
+                    resolve(readFile2(filePath));
+                }
+
+            }).then((res) => {
+                console.log("*******************")
+                console.log(res);
+                data = {
+                    "data": res,
+                    // "key": String(dirs.indexOf(item)),
+                    key,//使用文件名
+                }
+                //console.log(key)
+                console.log(encodeURI("http://47.94.216.106/api/test?key=" + key));
+                post(encodeURI("http://47.94.216.106/api/test?key=" + key), data).then((res) => {
+                    console.log(res)
+                }).catch((err) => {
+                    console.log(err)
+                })
+            }).catch((err) => {
+                console.log(err)
+            })
+        }
+    
+}
+
+
+
+
+module.exports ={
+    postData
+}
